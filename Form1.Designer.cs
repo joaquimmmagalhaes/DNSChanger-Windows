bf994c53f837efd3d9fdcb9422ecabc749ba3506
Joaquim Magalhães <magalhaes1915@gmail.com>
2017-08-20 15:50:40 +0100

    Initial push

---
+++ Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace DNSChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Server_One = new System.Windows.Forms.Button();
+            this.Adapters_ComboBox = new System.Windows.Forms.ComboBox();
+            this.Server_Two = new System.Windows.Forms.Button();
+            this.Server_Three = new System.Windows.Forms.Button();
+            this.Server_Four = new System.Windows.Forms.Button();
+            this.Reset_DNS = new System.Windows.Forms.Button();
+            this.Apply = new System.Windows.Forms.Button();
+            this.Cancel_Selection = new System.Windows.Forms.Button();
+            this.Primary_Server_Label_Title = new System.Windows.Forms.Label();
+            this.Secondary_Server_Label_Title = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Primary_Server_Label_Name = new System.Windows.Forms.Label();
+            this.Secondary_Server_Label_Name = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adapter";
+            // 
+            // Server_One
+            // 
+            this.Server_One.Location = new System.Drawing.Point(13, 79);
+            this.Server_One.Name = "Server_One";
+            this.Server_One.Size = new System.Drawing.Size(75, 23);
+            this.Server_One.TabIndex = 1;
+            this.Server_One.Text = "Google";
+            this.Server_One.UseVisualStyleBackColor = true;
+            this.Server_One.Click += new System.EventHandler(this.Server_One_Click);
+            // 
+            // Adapters_ComboBox
+            // 
+            this.Adapters_ComboBox.FormattingEnabled = true;
+            this.Adapters_ComboBox.Location = new System.Drawing.Point(60, 12);
+            this.Adapters_ComboBox.Name = "Adapters_ComboBox";
+            this.Adapters_ComboBox.Size = new System.Drawing.Size(180, 21);
+            this.Adapters_ComboBox.TabIndex = 2;
+            this.Adapters_ComboBox.SelectedIndexChanged += new System.EventHandler(this.Adapters_ComboBox_SelectedIndexChanged);
+            // 
+            // Server_Two
+            // 
+            this.Server_Two.Location = new System.Drawing.Point(94, 79);
+            this.Server_Two.Name = "Server_Two";
+            this.Server_Two.Size = new System.Drawing.Size(75, 23);
+            this.Server_Two.TabIndex = 3;
+            this.Server_Two.Text = "Google";
+            this.Server_Two.UseVisualStyleBackColor = true;
+            this.Server_Two.Click += new System.EventHandler(this.Server_Two_Click);
+            // 
+            // Server_Three
+            // 
+            this.Server_Three.Location = new System.Drawing.Point(13, 108);
+            this.Server_Three.Name = "Server_Three";
+            this.Server_Three.Size = new System.Drawing.Size(75, 23);
+            this.Server_Three.TabIndex = 4;
+            this.Server_Three.Text = "Google";
+            this.Server_Three.UseVisualStyleBackColor = true;
+            this.Server_Three.Click += new System.EventHandler(this.Server_Three_Click);
+            // 
+            // Server_Four
+            // 
+            this.Server_Four.Location = new System.Drawing.Point(94, 108);
+            this.Server_Four.Name = "Server_Four";
+            this.Server_Four.Size = new System.Drawing.Size(75, 23);
+            this.Server_Four.TabIndex = 5;
+            this.Server_Four.Text = "Google";
+            this.Server_Four.UseVisualStyleBackColor = true;
+            this.Server_Four.Click += new System.EventHandler(this.Server_Four_Click);
+            // 
+            // Reset_DNS
+            // 
+            this.Reset_DNS.Location = new System.Drawing.Point(13, 137);
+            this.Reset_DNS.Name = "Reset_DNS";
+            this.Reset_DNS.Size = new System.Drawing.Size(75, 23);
+            this.Reset_DNS.TabIndex = 6;
+            this.Reset_DNS.Text = "Reset DNS";
+            this.Reset_DNS.UseVisualStyleBackColor = true;
+            this.Reset_DNS.Click += new System.EventHandler(this.Reset_DNS_Click);
+            // 
+            // Apply
+            // 
+            this.Apply.Location = new System.Drawing.Point(13, 226);
+            this.Apply.Name = "Apply";
+            this.Apply.Size = new System.Drawing.Size(75, 23);
+            this.Apply.TabIndex = 7;
+            this.Apply.Text = "Apply";
+            this.Apply.UseVisualStyleBackColor = true;
+            this.Apply.Click += new System.EventHandler(this.Apply_Click);
+            // 
+            // Cancel_Selection
+            // 
+            this.Cancel_Selection.Location = new System.Drawing.Point(94, 226);
+            this.Cancel_Selection.Name = "Cancel_Selection";
+            this.Cancel_Selection.Size = new System.Drawing.Size(75, 23);
+            this.Cancel_Selection.TabIndex = 8;
+            this.Cancel_Selection.Text = "Cancel";
+            this.Cancel_Selection.UseVisualStyleBackColor = true;
+            this.Cancel_Selection.Click += new System.EventHandler(this.Cancel_Selection_Click);
+            // 
+            // Primary_Server_Label_Title
+            // 
+            this.Primary_Server_Label_Title.AutoSize = true;
+            this.Primary_Server_Label_Title.Location = new System.Drawing.Point(10, 186);
+            this.Primary_Server_Label_Title.Name = "Primary_Server_Label_Title";
+            this.Primary_Server_Label_Title.Size = new System.Drawing.Size(75, 13);
+            this.Primary_Server_Label_Title.TabIndex = 9;
+            this.Primary_Server_Label_Title.Text = "Primary Server";
+            // 
+            // Secondary_Server_Label_Title
+            // 
+            this.Secondary_Server_Label_Title.AutoSize = true;
+            this.Secondary_Server_Label_Title.Location = new System.Drawing.Point(10, 199);
+            this.Secondary_Server_Label_Title.Name = "Secondary_Server_Label_Title";
+            this.Secondary_Server_Label_Title.Size = new System.Drawing.Size(92, 13);
+            this.Secondary_Server_Label_Title.TabIndex = 10;
+            this.Secondary_Server_Label_Title.Text = "Secondary Server";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Select two servers";
+            // 
+            // Primary_Server_Label_Name
+            // 
+            this.Primary_Server_Label_Name.AutoSize = true;
+            this.Primary_Server_Label_Name.Location = new System.Drawing.Point(108, 185);
+            this.Primary_Server_Label_Name.Name = "Primary_Server_Label_Name";
+            this.Primary_Server_Label_Name.Size = new System.Drawing.Size(0, 13);
+            this.Primary_Server_Label_Name.TabIndex = 12;
+            // 
+            // Secondary_Server_Label_Name
+            // 
+            this.Secondary_Server_Label_Name.AutoSize = true;
+            this.Secondary_Server_Label_Name.Location = new System.Drawing.Point(108, 199);
+            this.Secondary_Server_Label_Name.Name = "Secondary_Server_Label_Name";
+            this.Secondary_Server_Label_Name.Size = new System.Drawing.Size(0, 13);
+            this.Secondary_Server_Label_Name.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 261);
+            this.Controls.Add(this.Secondary_Server_Label_Name);
+            this.Controls.Add(this.Primary_Server_Label_Name);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Secondary_Server_Label_Title);
+            this.Controls.Add(this.Primary_Server_Label_Title);
+            this.Controls.Add(this.Cancel_Selection);
+            this.Controls.Add(this.Apply);
+            this.Controls.Add(this.Reset_DNS);
+            this.Controls.Add(this.Server_Four);
+            this.Controls.Add(this.Server_Three);
+            this.Controls.Add(this.Server_Two);
+            this.Controls.Add(this.Adapters_ComboBox);
+            this.Controls.Add(this.Server_One);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "DNSChanger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Server_One;
+        private System.Windows.Forms.ComboBox Adapters_ComboBox;
+        private System.Windows.Forms.Button Server_Two;
+        private System.Windows.Forms.Button Server_Three;
+        private System.Windows.Forms.Button Server_Four;
+        private System.Windows.Forms.Button Reset_DNS;
+        private System.Windows.Forms.Button Apply;
+        private System.Windows.Forms.Button Cancel_Selection;
+        private System.Windows.Forms.Label Primary_Server_Label_Title;
+        private System.Windows.Forms.Label Secondary_Server_Label_Title;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Primary_Server_Label_Name;
+        private System.Windows.Forms.Label Secondary_Server_Label_Name;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+using System.Net;
+
+namespace DNSChanger
+{
+    public partial class Form1 : Form
+    {
+
+        private string Adapter_Selected = "";
+        private string Server_Primary = "";
+        private string Server_Secondary = "";
+        private string Server_One_Name = "Google";
+        private string Server_One_IP = "8.8.8.8";
+        private string Server_Two_Name = "OpenDNS";
+        private string Server_Two_IP = "208.67.222.222";
+        private string Server_Three_Name = "Comodo";
+        private string Server_Three_IP = "8.26.56.26";
+        private string Server_Four_Name = "SafeDNS";
+        private string Server_Four_IP = "195.46.39.39";
+
+        public Form1()
+        {
+            InitializeComponent();
+            Add_Info();
+            Primary_Server_Label_Title.Enabled = false;
+            Secondary_Server_Label_Title.Enabled = false;
+            Adapters_ComboBox.SelectedIndex = 0;
+        }
+
+
+        private void Add_Info()
+        {
+            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface adapter in adapters)
+            {
+                Adapters_ComboBox.Items.Add(adapter.Name);
+                IPAddressCollection dnsServers = adapter.GetIPProperties().DnsAddresses;
+                Console.WriteLine(adapter.Name);
+                foreach (IPAddress dns in dnsServers)
+                {
+                    Console.WriteLine(dns.ToString());
+                }
+            }
+
+            Server_One.Text = Server_One_Name;
+            Server_Two.Text = Server_Two_Name;
+            Server_Three.Text = Server_Three_Name;
+            Server_Four.Text = Server_Four_Name;
+        }
+
+        private void Server_One_Click(object sender, EventArgs e)
+        {
+            Selected_Server(Server_One_Name, Server_One_IP);
+        }
+
+        private void Server_Two_Click(object sender, EventArgs e)
+        {
+            Selected_Server(Server_Two_Name, Server_Two_IP);
+        }
+
+        private void Server_Three_Click(object sender, EventArgs e)
+        {
+            Selected_Server(Server_Three_Name, Server_Three_IP);
+        }
+
+        private void Server_Four_Click(object sender, EventArgs e)
+        {
+            Selected_Server(Server_Four_Name, Server_Four_IP);
+        }
+
+        private void Adapters_ComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Adapter_Selected = Adapters_ComboBox.SelectedItem.ToString();
+        }
+
+        private void Reset_DNS_Click(object sender, EventArgs e)
+        {
+            Run_Command("netsh interface ipv4 set dnsservers " + '"' + Adapter_Selected + '"' + " dhcp");
+        }
+
+        private void Apply_Click(object sender, EventArgs e)
+        {
+            Apply.Enabled = false;
+            if (Server_Secondary.CompareTo("") == 0)
+            {
+                Run_Command("netsh interface ipv4 set dnsservers " + '"' + Adapter_Selected + '"' + " static " + Server_Primary + " primary");
+                Apply.Enabled = true;
+                return;
+            }
+            Run_Command("netsh interface ipv4 set dnsservers " + '"' + Adapter_Selected + '"' + " static " + Server_Primary + " primary");
+            Run_Command("netsh interface ipv4 add dnsserver " + '"' + Adapter_Selected + '"' + " address=" + Server_Secondary + " index=2");
+            Apply.Enabled = true;
+        }
+
+        private void Run_Command(string command)
+        {
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+            cmd.StandardInput.WriteLine(command);
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+        }
+
+        private void Selected_Server(string Server_Name, string Server_IP)
+        {
+            if (Server_Primary.CompareTo("") == 0)
+            {
+                Server_Primary = Server_IP;
+                Primary_Server_Label_Title.Enabled = true;
+                Primary_Server_Label_Name.Text = Server_Name;
+            }
+            else if (Server_Secondary.CompareTo("") == 0)
+            {
+                Server_Secondary = Server_IP;
+                Secondary_Server_Label_Title.Enabled = true;
+                Secondary_Server_Label_Name.Text = Server_Name;
+            }
+        }
+
+        private void Cancel_Selection_Click(object sender, EventArgs e)
+        {
+            Server_Primary = "";
+            Primary_Server_Label_Title.Enabled = false;
+            Primary_Server_Label_Name.Text = "";
+            Server_Secondary = "";
+            Secondary_Server_Label_Title.Enabled = false;
+            Secondary_Server_Label_Name.Text = "";
+        }
+    }
+}
